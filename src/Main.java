8e4ff86998ca04c1a08c12cbf0eb4e4e882a7396
DenisKrinitsyn <skylinekrin@gmail.com>
2022-11-27 21:25:26 +0500

    Add method firstRum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE /2;
+
+    static float[] arr = new float[SIZE];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        firstRum();
+
     }
+
+    public static void firstRum(){
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = (float)(arr[i]*Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
 }
